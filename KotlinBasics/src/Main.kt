5a2f2bdd4f542d3b32c929ea33d24cc2800f163f
Kartikay Singh <kartikays@Kartikays-MacBook-Pro.local>
2021-12-05 19:13:38 +0530

    Basics of variables, strings interplation

--- KotlinBasics/src/Main.kt
+++ KotlinBasics/src/Main.kt
@@ -1,4 +1,26 @@
 fun main(args: Array<String>) {
-    print("Kotlin says: ")
     println("Hello World!")
+
+    // Variable types, arithmetics
+    var name: String
+    name = "Kartikay Singh"
+    println("Name: $name")
+
+    val daily: Int = 50
+    val monthly: Int = daily * 30
+    println("Salary: ₹$monthly.")
+
+    val apples: Int = 6
+    val oranges: Int = 5
+    val total = apples + oranges
+    println("$apples apples + $oranges oranges = $total fruit(s)")
+
+    val weeks: Int = 130
+    val years: Double = weeks / 52.0
+
+    println(years)
+
+    // String operations
+    println("$weeks weeks means $years years")
+    println("Quarter of total $apples apples is ${apples / 4.0}")
 }
